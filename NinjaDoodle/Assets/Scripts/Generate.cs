d7e9383621a07778e966920190e72926e99b9440
Sergio González Polán <sergpolan@gmail.com>
2015-06-17 15:43:14 +0200

    Añadida pantalla de inicio

--- NinjaDoodle/Assets/Scripts/Generate.cs
+++ NinjaDoodle/Assets/Scripts/Generate.cs
@@ -3,16 +3,21 @@ using System.Collections;
 
 public class Generate : MonoBehaviour {
 
-	public GameObject walls;
+	public GameObject wall_left;
+	public GameObject wall_right;
 
 	// Use this for initialization
 	void Start () {
 		print ("Walls creating.");
+		print ("Ancho de pantalla " + Screen.width);
+		print ("Alto de pantalla " + Screen.height);
 		InvokeRepeating ("CreatingWall", 1f, 0.65f);
 	}
 	
 	void CreatingWall()
 	{
-		Instantiate (walls);
+		Instantiate (wall_left, new Vector3(Screen.width - Screen.width + 10 ,11.25f,0),  Quaternion.identity);	
+		Instantiate (wall_right, new Vector3(Screen.width/2 ,11.25f,0),  Quaternion.identity);	
+
 	}
 }
---
+++ NinjaDoodle/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuScript : MonoBehaviour {
+
+	void OnGUI()
+	{
+		const int buttonWidth = 100;
+		const int buttonHeight = 128;
+		
+
+		Rect buttonRect = new Rect(
+			Screen.width / 2 - (buttonWidth / 2),
+			(2 * Screen.height / 3) - (buttonHeight / 2),
+			buttonWidth,
+			buttonHeight
+			);
+		
+		if(GUI.Button(buttonRect,"Juega!"))
+		{
+			Application.LoadLevel("MainGame");
+		}
+	}
+}
