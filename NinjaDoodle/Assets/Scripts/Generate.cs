b45d134af0ffd33df1c7039f8ffe23ea6f54d316
Sergio González Polán <sergpolan@gmail.com>
2015-06-21 03:04:16 +0200

    Se generan las guias donde choca el personaje, cuando choca cambia la animacion y desactiva el muro para poder aplicarle la fuerza.

--- NinjaDoodle/Assets/Scripts/Generate.cs
+++ NinjaDoodle/Assets/Scripts/Generate.cs
@@ -5,31 +5,38 @@ public class Generate : MonoBehaviour {
 
 	public GameObject wall_left;
 	public GameObject wall_right;
+	public GameObject vWall_left;
+	public GameObject vWall_right;
 	public GameObject player;
+	private Renderer wallRender;
+
 	Vector3 v3WallLeft = new Vector3(0.01f, 1.2f, 1);
 	Vector3 v3WallRight = new Vector3(0.99f, 1.2f, 1);
+	Vector3 v3VerticalWallLeft = new Vector3(0.17f, 1.2f, 1);
+	Vector3 v3VerticalWallRight = new Vector3(0.83f, 1.2f, 1);
 	Vector3 v3Player = new Vector3(0.5f, 0.5f, 1);
 
-	// Use this for initialization
 	void Start () {
-		print ("Walls creating.");
-		print ("Ancho de pantalla " + Screen.width);
-		print ("Alto de pantalla " + Screen.height);
 		Invoke ("CreatingPlayer", 0.5f);
+		Invoke ("CreatinVerticalWall", 0.6f);
 		InvokeRepeating ("CreatingWall", 1f, 0.6f);
 	}
 	
 	void CreatingWall()
 	{
-		print ("Creando el muro");
 		Instantiate (wall_left, Camera.main.ViewportToWorldPoint (v3WallLeft), Quaternion.identity);
 		Instantiate (wall_right, Camera.main.ViewportToWorldPoint (v3WallRight), Quaternion.identity);
 	}
 
 	void CreatingPlayer()
 	{
-		print ("Creando el personaje");
 		Instantiate (player, Camera.main.ViewportToWorldPoint (v3Player), Quaternion.identity);
 
+	}
+
+	void CreatinVerticalWall()
+	{
+		Instantiate (vWall_left, Camera.main.ViewportToWorldPoint (v3VerticalWallLeft), Quaternion.identity);
+		Instantiate (vWall_right, Camera.main.ViewportToWorldPoint (v3VerticalWallRight), Quaternion.identity);
 	}
 }
--- NinjaDoodle/Assets/Scripts/PlayerMovement.cs
+++ NinjaDoodle/Assets/Scripts/PlayerMovement.cs
@@ -3,21 +3,30 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour
 {
-	public Vector2 jumpForce = new Vector2 (300, 0);
+	public Vector2 jumpForce;
+	private Animator animator;
+	private bool moveRight = true;
+	private GameObject lastWallHitted;
 		
+	void Start()
+	{
+		animator = GetComponent<Animator>();
+	}
+
 	void Update ()
 	{
 		if (Input.GetMouseButtonDown (0)) {
 			Debug.Log ("Pressed left click.");
-			GetComponent<Rigidbody2D>().velocity = Vector2.zero;
-			GetComponent<Rigidbody2D>().AddForce (jumpForce);
+			playerJump();
 		}
-		if (Input.GetMouseButtonDown (1))
+		if (Input.GetMouseButtonDown (1)) {
 			Debug.Log ("Pressed right click.");
-		if (Input.GetMouseButtonDown (2))
+			animator.SetBool("glide", true);
+		}
+		if (Input.GetMouseButtonDown (2)) {
 			Debug.Log ("Pressed middle click.");
+		}
 
-		//Muere cuando se sale de la pantalla
 		Vector2 screenPosition = Camera.main.WorldToScreenPoint(transform.position);
 		if (screenPosition.y > Screen.height || screenPosition.y < 0)
 		{
@@ -28,13 +37,48 @@ public class PlayerMovement : MonoBehaviour
 	//Metodo que cuando colisiona muere
 	void OnCollisionEnter2D(Collision2D other)
 	{
-		Die();
+		if (other.gameObject.tag == "WallIdle") {
+			//Destroy(other.gameObject);
+			if(lastWallHitted != null)
+					lastWallHitted.SetActive (true);
+			lastWallHitted = other.gameObject;
+			lastWallHitted.SetActive (false);
+			flipAnimation ();
+			print ("Choco con el muro que no se mueve");
+		}
+		
 	}
 	
 	void Die()
 	{
 		Debug.Log ("Muere el player1------------------------------");
 		Application.LoadLevel(Application.loadedLevel);
+	}
+
+	//Añade una fuerza al personaje dependiendo de si el personaje se
+	// tiene que mover a la derecha o a la izquierda, y dependiendo de esto
+	// rota la animacion
+	void playerJump ()
+	{
+		GetComponent<Rigidbody2D> ().velocity = Vector2.zero;
+		if (moveRight) {
+			jumpForce = new Vector2 (10, 0);
+			print("Vamos derecha");
+		} else {
+			jumpForce = new Vector2 (-10, 0);
+			print("Vamos izquierda");
+		}
+		GetComponent<Rigidbody2D> ().AddForce (jumpForce, ForceMode2D.Impulse);
+	}
+
+	//Cambia el sentido de la animacion, haciendo un flip de 180
+	void flipAnimation ()
+	{
+		moveRight = !moveRight;
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;  
+		print ("Cambia la animacion");
 	}
 }
 
